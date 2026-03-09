c884b27dae37a3b82a9a315881acdbe3760e1c6a
Khalid issil <issilk16@email.com>
2026-03-09 13:37:30 +0000

    Update CI/CD pipeline and Kubernetes deployment

---
+++ terraform/main.tf
@@ -0,0 +1,85 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "devops_sg" {
+  name        = "devops-security-group"
+  description = "Security group for DevOps project"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Kubernetes API Server"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description = "Kubelet API"
+    from_port   = 10250
+    to_port     = 10250
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Flannel VXLAN"
+    from_port   = 8472
+    to_port     = 8472
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Flask App"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "master" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "k8s-master"
+  }
+}
+
+resource "aws_instance" "worker" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "k8s-worker"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "eu-north-1"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "key_name" {
+  default = "devops-key"
+}
+
+variable "ami_id" {
+  default = "ami-0c1ac8a41498c1a9c"
+}
